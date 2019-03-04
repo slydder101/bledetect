7eb48f5e760fc4f94de2d85cac482a246487de15
Gyula FARKAS <gfarkas@bitdefender.com>
2019-03-04 10:15:42 +0200

    updated device detection

--- src/bledetect.c
+++ src/bledetect.c
@@ -8,6 +8,9 @@
 
 int main(int argc, char **argv)
 {
+    char* source = NULL;
+    if (argc > 1)
+        source = argv[1];
     inquiry_info *ii = NULL;
     int max_rsp, num_rsp;
     int dev_id, sock, len, flags;
@@ -15,25 +18,30 @@ int main(int argc, char **argv)
     char addr[19] = { 0 };
     char name[248] = { 0 };
 
-    dev_id = hci_get_route(NULL);
+    if (source)
+        dev_id = hci_devid(source);
+    else
+        dev_id = hci_get_route(NULL);
     sock = hci_open_dev( dev_id );
+
     if (dev_id < 0 || sock < 0) {
         perror("opening socket");
         exit(1);
-    }
+    } else
+        printf("Device socket id:%d\n", dev_id);
 
     len  = 8;
     max_rsp = 255;
     flags = IREQ_CACHE_FLUSH;
     ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
-    
+
     num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
     if( num_rsp < 0 ) perror("hci_inquiry");
 
     for (i = 0; i < num_rsp; i++) {
         ba2str(&(ii+i)->bdaddr, addr);
         memset(name, 0, sizeof(name));
-        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
+        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name),
             name, 0) < 0)
         strcpy(name, "[unknown]");
         printf("%s  %s\n", addr, name);
